8bcf9517c16e318f0b8bfeaf5dc2e414309747ed
Marconi Maciel Guimarães Juniot <102702355+MarconiJunior@users.noreply.github.com>
2022-09-30 20:50:10 -0300

    Create Exs3.java

---
+++ Exs3.java
@@ -0,0 +1,3 @@
+//While == é usada quando não sabemos a quantidade de vezes que a repetição será utilizada, a repetição continuará até a condição ser vedadeira
+//Do While == O comando é baseado no While porém a validação da estrutura é feita depois do comando
+//For == é usado quando se sabe a quantidade de vezes que a repetição será utilizada.
