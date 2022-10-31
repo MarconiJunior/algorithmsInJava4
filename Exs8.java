a6d696d5ad4fd219d36bef42e6cf47890b3c464e
Marconi Maciel Guimarães Juniot <102702355+MarconiJunior@users.noreply.github.com>
2022-10-31 19:50:16 -0300

    Create Exs8.java

---
+++ Exs8.java
@@ -0,0 +1,16 @@
+public class projeto {
+  public static void main(String[] args) {
+      for (int i = 1; i <= 100; i++) {
+          int c = 0;
+          for (int j = 1; j <= i; j++) {
+              if (i % j == 0) {
+                  c++;
+              }
+          }
+          if (c <= 2) {
+              System.out.println(i);
+          }
+      }
+  }
+}
+    
